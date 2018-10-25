09c3f5dc1c85453ee4ccf2a716fb14f6dd639a9b
Zhacro <38124549+Zhacro@users.noreply.github.com>
2018-10-25 10:40:11 +0900

    Add files via upload

---
+++ SanaButtonDownloader.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SanaButtonDownloader
+{
+	class SanaButtonDownloader
+	{
+		const string soundRoot = "https://www.natorisana.love/sounds/";
+		static void Main(string[] args)
+		{
+			var wc = new WebClient();
+			string html = Encoding.UTF8.GetString(wc.DownloadData("https://www.natorisana.love"));
+			if (!CheckUpdate(html))
+			{
+				Console.WriteLine("更新はありません。");
+				return;
+			}
+			Console.Write("OutputDirectory; ");
+			var output = Console.ReadLine();
+			var re = new Regex(@"<button type=""button"" class=""sounds"" data-file=""(.+?)"">.+?</button>");
+			foreach (var item in re.Matches(html).Cast<Match>().Select(i => i.Groups.Cast<Group>().ElementAt(1).Value))
+			{
+				Process(item, output);
+				Console.WriteLine("1回やりました");
+				return;
+			}
+		}
+
+		static void Process(string fileName,string targetDir)
+		{
+			var wc = new WebClient();
+			var target = $"{targetDir}\\{fileName}.mp3";
+			if (File.Exists(target)) return;
+			if (fileName.Contains("/")) Directory.CreateDirectory(targetDir + "\\" + fileName.Split('/')[0]);
+			wc.DownloadFile(soundRoot + fileName+".mp3", target);
+		}
+
+		static bool CheckUpdate(string html)
+		{
+			if (File.Exists("LastUpdate.txt"))
+			{
+				var date = File.ReadAllText("LastUpdate.txt");
+				var dateReg = new Regex(@"\d?\d/\d?\d");
+				if (dateReg.Match(html).Value == date) return false;
+				Console.WriteLine("更新を確認しました。");
+				return true;
+			}
+			else
+			{
+				Console.WriteLine("前回の使用記録が存在しません。作成します。");
+				var log = File.CreateText("LastUpdate.txt");
+				var dateReg = new Regex(@"\d?\d/\d?\d");
+				log.Write(dateReg.Match(html).Value);
+				log.Close();
+				return true;
+			}
+		}
+	}
+}
